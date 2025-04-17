be0e889b0f0fb2351ddfbefc29c958062ebd42e5
Taras Basiuk <BasiukTV@gmail.com>
2025-04-17 14:38:12 -0400

    Making some changes to the hello world java app.

--- new_folder/hello_world.java
+++ new_folder/hello_world.java
@@ -1,10 +1,15 @@
 public class hello_world {
+
     public static void main(String[] args) {
-        var name = new String("Bob");
-        System.out.println("Hello, " + name + "! Nice to meet you!");
+        var name = new String("Bobert");
+        System.out.println("Hello, " + name + "! Nice to meet you! How are you doing?");
     }
 
     private static sub(int a, int b) {
         return a - b;
+    }
+
+    private static mul(int a, int b) {
+        return a * b;
     }
 }
