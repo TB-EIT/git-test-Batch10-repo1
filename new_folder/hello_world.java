ab20ac6f7a53878784cb38bb13c1631fcb79c4e6
TB-EIT <133935898+TB-EIT@users.noreply.github.com>
2025-04-17 14:34:13 -0400

    Update hello_world.java

--- new_folder/hello_world.java
+++ new_folder/hello_world.java
@@ -1,5 +1,6 @@
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var name = new String("Taras");
+        System.out.println("Hello, " + name + "!");
     }
 }
